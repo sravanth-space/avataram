71efba24eaa2166ac125570a907b1ca3278a2d21
sravanthbaratam <sravanthbaratam@gmail.com>
2023-06-22 10:46:57 +0100

    Upated Mouth

--- src/components/mouth/normal.tsx
+++ src/components/mouth/normal.tsx
@@ -3,27 +3,89 @@ import React from "react";
 export default function Normal() {
   return (
     <>
-      <g clipPath="url(#clip4_274_4378)">
+      <g clip-path="url(#clip0_740_796)">
         <path
-          d="M27.9297 46C27.9297 45.4477 28.3774 45 28.9297 45H38.0725C38.6248 45 39.0725 45.4477 39.0725 46V46C39.0725 48.7614 36.834 51 34.0725 51H32.9297C30.1683 51 27.9297 48.7614 27.9297 46V46Z"
+          d="M30 46C30 45.4477 30.4477 45 31 45H40.1429C40.6951 45 41.1429 45.4477 41.1429 46V46C41.1429 48.7614 38.9043 51 36.1429 51H35C32.2386 51 30 48.7614 30 46V46Z"
           fill="#66253C"
         />
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
-          d="M35.7558 50.7081C35.2298 50.8961 34.6631 50.9985 34.0725 50.9985H32.9297C30.2352 50.9985 28.0385 48.8671 27.9336 46.1984C28.6976 45.9125 29.8316 45.9454 30.951 45.9779C31.3066 45.9882 31.6608 45.9985 32.0011 45.9985C34.2103 45.9985 36.0011 47.5655 36.0011 49.4985C36.0011 49.9236 35.9145 50.3311 35.7558 50.7081Z"
+          d="M37.8261 50.7096C37.3001 50.8976 36.7335 51 36.1429 51H35C32.3055 51 30.1088 48.8687 30.0039 46.1999C30.7679 45.914 31.902 45.9469 33.0214 45.9794C33.377 45.9897 33.7311 46 34.0714 46C36.2806 46 38.0714 47.567 38.0714 49.5C38.0714 49.9252 37.9848 50.3326 37.8261 50.7096Z"
           fill="#B03E67"
         />
         <path
-          d="M29 45H39V46C39 46.5523 38.5523 47 38 47H30C29.4477 47 29 46.5523 29 46V45Z"
+          d="M31.0714 45H41.0714V46C41.0714 46.5523 40.6237 47 40.0714 47H32.0714C31.5192 47 31.0714 46.5523 31.0714 46V45Z"
           fill="white"
         />
       </g>
       <defs>
-        <clipPath id="clip4_274_4378">
+        <clipPath id="clip0_740_796">
           <rect width="64" height="64" fill="white" />
         </clipPath>
       </defs>
     </>
   );
 }
+
+<svg
+  width="64"
+  height="64"
+  viewBox="0 0 64 64"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g filter="url(#filter0_d_736_754)">
+    <path
+      d="M30 46C30 45.4477 30.4477 45 31 45H40.1429C40.6951 45 41.1429 45.4477 41.1429 46V46C41.1429 48.7614 38.9043 51 36.1429 51H35C32.2386 51 30 48.7614 30 46V46Z"
+      fill="#66253C"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M37.8261 50.7096C37.3001 50.8976 36.7335 51 36.1429 51H35C32.3055 51 30.1088 48.8687 30.0039 46.1999C30.7679 45.914 31.902 45.9469 33.0214 45.9794C33.377 45.9897 33.7311 46 34.0714 46C36.2806 46 38.0714 47.567 38.0714 49.5C38.0714 49.9252 37.9848 50.3326 37.8261 50.7096Z"
+      fill="#B03E67"
+    />
+    <path
+      d="M31.0714 45H41.0714V46C41.0714 46.5523 40.6237 47 40.0714 47H32.0714C31.5192 47 31.0714 46.5523 31.0714 46V45Z"
+      fill="white"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_d_736_754"
+      x="26"
+      y="45"
+      width="19.1429"
+      height="14"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="2" />
+      <feComposite in2="hardAlpha" operator="out" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow_736_754"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect1_dropShadow_736_754"
+        result="shape"
+      />
+    </filter>
+  </defs>
+</svg>;
